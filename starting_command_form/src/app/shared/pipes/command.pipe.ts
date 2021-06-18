32675866b55e41f167a6a66f47a312eb766100d9
Yaroslav Tarkan <yaroslav.tarkan@intel.com>
2021-06-18 15:31:51 +0300

    [FORM] [DEPLOY] Aligned with Starter CLI (#16)

--- starting_command_form/src/app/shared/pipes/command.pipe.ts
+++ starting_command_form/src/app/shared/pipes/command.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { ICommandConfig, OperatingSystems, StartTypes } from '../models/command-constructor-form';
+import { ICommandConfig, StartTypes } from '../models/command-constructor-form';
 import { commandDefaults } from '../constants';
 import { DockerCommandParametersBuilder, PythonCommandParametersBuilder } from '../models/command-parameter';
 
@@ -12,21 +12,14 @@ export class CommandPipe implements PipeTransform {
     [StartTypes.DOCKER]: commandDefaults.dockerRunCommand,
   };
 
-  private readonly lineContinueSymbolMap = {
-    [OperatingSystems.LINUX]: commandDefaults.unixLineContinuationSymbol,
-    [OperatingSystems.MAC_OS]: commandDefaults.unixLineContinuationSymbol,
-    [OperatingSystems.WINDOWS]: commandDefaults.windowsLineContinuationSymbol,
-  };
-
   transform(commandConfig: ICommandConfig): string {
-    const { startWith, os, devices, httpProxy, httpsProxy, noProxy } = commandConfig;
+    const { startWith, devices, httpProxy, httpsProxy, noProxy } = commandConfig;
     const commandBuilder = startWith === StartTypes.DOCKER ? new DockerCommandParametersBuilder() : new PythonCommandParametersBuilder();
     commandBuilder.addDevicesParameters(devices);
     commandBuilder.addProxyParameters(httpProxy, httpsProxy, noProxy);
     const commandExecutable = this.commandExecutableMap[startWith];
-    const lineContinueSymbol = this.lineContinueSymbolMap[os];
-    const parametersString = commandBuilder.parameters.map((parameter) => parameter.toString()).join(` ${lineContinueSymbol}\n\t`);
-    return `${commandExecutable} ${lineContinueSymbol}\n\t${parametersString}`;
+    const parametersString = commandBuilder.parameters.map((parameter) => parameter.toString()).join(' ');
+    return `${commandExecutable} ${parametersString}`;
   }
 
 }
