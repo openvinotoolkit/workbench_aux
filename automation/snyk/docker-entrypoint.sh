b63567b6cf822ac2ac3f61e5627383c6d373e59f
Andrej Kashchikhin <andrey.kashchikhin@intel.com>
2021-06-30 16:33:21 +0300

    correct project path

--- automation/snyk/docker-entrypoint.sh
+++ automation/snyk/docker-entrypoint.sh
@@ -4,19 +4,25 @@ OUTPUT_FILE=snyk-result.json
 
 python3 -m pip install setuptools wheel
 
+ls -la
+
 # Install project dependencies
-find ${PROJECT_FOLDER}/ \
+find ./ \
   -name 'requirements*.txt' \
   -exec python3 -m pip install -r {} \;
 
-find ${PROJECT_FOLDER}/ \
+find ./ \
   -name 'requirements*.txt' \
   -exec cat {} \; &> requirements_prod.txt
+
+cat requirements_prod.txt
 
 snyk test --json \
           --file=requirements_prod.txt \
           --command=python3 \
           --package-manager=pip \
           --project-name=Workbench_starter > ${OUTPUT_FILE}
+
+cat ${OUTPUT_FILE}
 
 exit $?
