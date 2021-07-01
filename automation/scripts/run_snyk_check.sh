06d6cd98c4faac2bba66d173f598724caa623f9b
Andrej Kashchikhin <andrey.kashchikhin@intel.com>
2021-07-01 10:28:40 +0300

    exit

--- automation/scripts/run_snyk_check.sh
+++ automation/scripts/run_snyk_check.sh
@@ -51,4 +51,6 @@ docker run \
   -v "${PROJECT_PATH}:/app" \
   --env COMMAND="pip install -r requirements_prod.txt" \
   snyk/snyk:python-3.9 \
-  snyk test --json --file=requirements_prod.txt --package-manager=pip
+  snyk test --json --file=requirements_prod.txt --package-manager=pip > ${OUTPUT_FILE} 2>&1
+
+exit 0
