0d9d5e95ea8777f0b69148abcbe851ab763cbaad
Alexander Demidovskij <alexander.demidovskij@intel.com>
2021-10-21 18:24:23 +0300

    [FORM] Added instructions for Docker and Python (#42)

--- starting_command_form/src/app/shared/components/result-section/result-section.component.ts
+++ starting_command_form/src/app/shared/components/result-section/result-section.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { Devices, ICommandConfig, OperatingSystems, StartTypes } from '../../models/command-constructor-form';
 import { commandDefaults } from '../../constants';
 import { MessagesService } from '../../services/messages.service';
@@ -9,7 +9,7 @@ import { MessagesService } from '../../services/messages.service';
   styleUrls: [ './result-section.component.scss' ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ResultSectionComponent {
+export class ResultSectionComponent implements OnInit {
   @Input()
   public commandConfig: ICommandConfig;
 
@@ -18,12 +18,15 @@ export class ResultSectionComponent {
   public OperatingSystems = OperatingSystems;
 
   public readonly commands = {
-    installPythonWrapper: `python3 -m pip install -U ${commandDefaults.pythonWrapperName}`,
     pullImage: `docker pull ${commandDefaults.dockerImageWithTag}`,
   };
   public readonly resultStepsMessages = this.messagesService.messages.resultSteps;
 
   constructor(private messagesService: MessagesService) {}
+
+  ngOnInit(){
+    console.log(this.commandConfig);
+  }
 
   get installDockerInstructionsContent(): string {
     const { os } = this.commandConfig;
@@ -35,5 +38,30 @@ export class ResultSectionComponent {
       return installDockerInstructions.macOS;
     }
     return installDockerInstructions.linux;
+  }
+
+  get installPythonCommand() {
+    const { os } = this.commandConfig;
+    const isWindows = os === OperatingSystems.WINDOWS; 
+    const pythonBinaryName = `python${isWindows? '': '3'}`;
+    return `${pythonBinaryName} -m pip install -U ${commandDefaults.pythonWrapperName}`
+  }
+
+  get allocateMemoryForDockerCommand() {
+    const { os } = this.commandConfig;
+    const isWindows = os === OperatingSystems.WINDOWS;
+    return isWindows?
+          this.messagesService.messages.resultSteps.descriptions.allocateMemoryForDocker.windows
+          : this.messagesService.messages.resultSteps.descriptions.allocateMemoryForDocker.macOS;
+  }
+
+  get installPipCommand() {
+    const { os } = this.commandConfig;
+    const isWindows = os === OperatingSystems.WINDOWS;
+    const pythonBinaryName = `python${isWindows? '': '3'}`;
+    const pipCommand =  `${pythonBinaryName} -m pip --version`
+    return `${this.messagesService.messages.resultSteps.descriptions.installPip}` +
+           ` "${pipCommand}". `+
+           `${this.messagesService.messages.resultSteps.descriptions.checkPipVersion}`;
   }
 }
